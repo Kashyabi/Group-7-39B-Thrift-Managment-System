0ace76e2accbd32429dc2a6191e1614630afb2c0
nirjala khanal9 <nirjalakhanal9@gmail.com>
2026-06-19 17:00:00 +0545

    Section 2 changed

--- src/view/Sales.java
+++ src/view/Sales.java
@@ -72,7 +72,7 @@ private void loadSalesData() {
         jPanel3.setBackground(new java.awt.Color(170, 218, 172));
 
         jButton1.setBackground(new java.awt.Color(170, 218, 172));
-        jButton1.setFont(new java.awt.Font("Arial", 1, 20)); // NOI18N
+        jButton1.setFont(new java.awt.Font("Arial Black", 0, 20)); // NOI18N
         jButton1.setText("Add Product");
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
